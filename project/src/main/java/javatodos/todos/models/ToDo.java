c0aa8500fc9669b26d87a891f337af2d3c265011
Jeff Lapp <lappjeff20@gmail.com>
2019-05-17 11:21:56 -0400

    Created Auditable class

--- project/src/main/java/javatodos/todos/models/ToDo.java
+++ project/src/main/java/javatodos/todos/models/ToDo.java
@@ -1,5 +1,17 @@
 package javatodos.todos.models;
 
-public class ToDo
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class ToDo extends Auditable
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long todoid;
+
+	@Column(nullable = false)
+	private String description;
+
+	private
 }
