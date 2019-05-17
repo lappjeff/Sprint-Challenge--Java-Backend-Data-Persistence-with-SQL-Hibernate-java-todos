4b5d6e5ac8c08f0576489f48dbdfad5af2ec18eb
Jeff Lapp <lappjeff20@gmail.com>
2019-05-17 11:49:26 -0400

    Set up auth files for user, userRoles, and role

--- project/src/main/java/javatodos/todos/models/User.java
+++ project/src/main/java/javatodos/todos/models/User.java
@@ -1,10 +1,112 @@
 package javatodos.todos.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User
+public class User extends Auditable
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long userid;
+
+	private String username;
+
+	private String password;
+
+	@OneToMany(mappedBy = "user",
+			   cascade = CascadeType.ALL,
+			   orphanRemoval = true,
+			   fetch = FetchType.LAZY)
+	@JsonIgnoreProperties("user")
+	private List<ToDo> todos = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user",
+			   cascade = CascadeType.ALL,
+			   orphanRemoval = true)
+	private List<UserRoles> userRoles = new ArrayList<>();
+
+	public User()
+	{
+	}
+
+	public User(String username, String password, List<UserRoles> userRoles)
+	{
+		setUsername(username);
+		setPassword(password);
+		for(UserRoles ur: userRoles)
+		{
+			ur.setUser(this);
+		}
+		this.userRoles = userRoles;
+	}
+
+	public long getUserid()
+	{
+		return userid;
+	}
+
+	public void setUserid(long userid)
+	{
+		this.userid = userid;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		this.password = passwordEncoder.encode(password);
+	}
+
+	public List<ToDo> getTodos()
+	{
+		return todos;
+	}
+
+	public void setTodos(List<ToDo> todos)
+	{
+		this.todos = todos;
+	}
+
+	public List<UserRoles> getUserRoles()
+	{
+		return userRoles;
+	}
+
+	public void setUserRoles(List<UserRoles> userRoles)
+	{
+		this.userRoles = userRoles;
+	}
+
+	public List<SimpleGrantedAuthority> getAuthority()
+	{
+		List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+		for (UserRoles r : this.userRoles)
+		{
+			String myRole = "ROLE_" + r.getRole().getRolename().toUpperCase();
+			rtnList.add(new SimpleGrantedAuthority(myRole));
+		}
+		return rtnList;
+	}
 }
