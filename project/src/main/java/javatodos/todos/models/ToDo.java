04fba692737d055b7557069b9d183518d7bd70d8
Jeff Lapp <lappjeff20@gmail.com>
2019-05-17 11:28:45 -0400

    Filled out ToDo class

--- project/src/main/java/javatodos/todos/models/ToDo.java
+++ project/src/main/java/javatodos/todos/models/ToDo.java
@@ -1,6 +1,11 @@
 package javatodos.todos.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "todos")
@@ -13,5 +18,59 @@ public class ToDo extends Auditable
 	@Column(nullable = false)
 	private String description;
 
-	private
+	private boolean completed = false;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userid")
+	@JsonIgnoreProperties("todos")
+	private User user;
+
+	public ToDo()
+	{
+	}
+
+	public ToDo(String description)
+	{
+		this.description = description;
+	}
+
+	public long getTodoid()
+	{
+		return todoid;
+	}
+
+	public void setTodoid(long todoid)
+	{
+		this.todoid = todoid;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public boolean isCompleted()
+	{
+		return completed;
+	}
+
+	public void setCompleted(boolean completed)
+	{
+		this.completed = completed;
+	}
+
+	public User getUser()
+	{
+		return user;
+	}
+
+	public void setUser(User user)
+	{
+		this.user = user;
+	}
 }
---
+++ project/src/main/java/javatodos/todos/models/User.java
@@ -0,0 +1,10 @@
+package javatodos.todos.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User
+{
+}
